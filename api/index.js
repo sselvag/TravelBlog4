3d8ebaf4f6e3d1e0a33172aea4a1a2d90c47dc4e
Swaathy <64749332+sselvag@users.noreply.github.com>
2023-10-18 16:16:21 -0400

    fixed syntax errors

--- api/index.js
+++ api/index.js
@@ -47,10 +47,10 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
-app.use(express.static(path.join(__dirname, "/client")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(_dirname, '/client/build', 'index.html'))
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 });
 
 app.listen(process.env.PORT || 5000, () => {
