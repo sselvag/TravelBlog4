a6d1c8bd483acb5494041b04bab18adbed23199b
Swaathy <64749332+sselvag@users.noreply.github.com>
2023-10-18 15:34:17 -0400

    updating dependencies

--- api/client/src/index.js
+++ api/client/src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ContextProvider } from "./Context/Context";
-import {disableReactDevTools} from "@fvilers/disable-react-devtools";
+// import {disableReactDevTools} from "@fvilers/disable-react-devtools";
 
-if (process.env.NODE_ENV === 'production') disableReactDevTools();
+// if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
